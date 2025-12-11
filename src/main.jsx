072c9b0d1b846a9da559471a97f8137f3e604151
AlvaroEMatos <handolfwanbell@gmail.com>
2025-12-11 14:16:52 -0300

    fix HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename='/secretaria-virtual'>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
